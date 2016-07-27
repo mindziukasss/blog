4585cd09def95f4e99d9b1c7098d1cfd370b83bb
mindziukasss <mindziukasss@gmail.com>
2016-07-27 16:30:12 +0300

    Add comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,16 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
+
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
 		@post = Post.new
+	end
+
+	def show
+		@post = Post.find(params[:id])
 	end
 
 	def create
@@ -17,32 +23,30 @@ class PostsController < ApplicationController
 		end
 	end
 
-	def show
-	@post = Post.find(params[:id])
-	end
-
 	def edit
-			@post = Post.find(params[:id])
+		@post = Post.find(params[:id])
 	end
+
 	def update
-			@post = Post.find(params[:id])
-			if @post.update(params[:post].permit(:title, :body))
-				redirect_to @post
-			else
-				render 'edit'
-			end
-		end
+		@post = Post.find(params[:id])
 
-		def destroy
-			@post = Post.find(params[:id])
-			@post.destroy
-
-			redirect_to root_path
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
 		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
 
 	private
-		def post_params
-			params.require(:post).permit(:title, :body)
-		end
 
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
 end
